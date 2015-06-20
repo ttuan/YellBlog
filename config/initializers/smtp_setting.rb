243fd847e0f4c6bf98a4c7c162013540d69c71ff
tuan <tuantv.nhnd@gmail.com>
2015-06-20 11:58:03 +0700

    lkda;jfs

--- config/initializers/smtp_setting.rb
+++ config/initializers/smtp_setting.rb
@@ -1,19 +1,19 @@
-ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address        => 'smtp.gmail.com',
-    :port           => 587,
-    :domain         => 'gmail.com',
-    :authentication => :plain,
-    :user_name      => 'yellblog.register@gmail.com',
-    :password       => 'Haianhanh1'
-}
-
 # ActionMailer::Base.smtp_settings = {
 #     :enable_starttls_auto => true,
 #     :address        => 'smtp.gmail.com',
 #     :port           => 587,
 #     :domain         => 'gmail.com',
 #     :authentication => :plain,
-#     :user_name      => 'pizika005@gmail.com',
-#     :password       => 'HAI ANHANH'
+#     :user_name      => 'yellblog.register@gmail.com',
+#     :password       => 'Haianhanh1'
 # }
+
+ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :domain         => 'gmail.com',
+    :authentication => :plain,
+    :user_name      => 'pizika005@gmail.com',
+    :password       => 'HAI ANHANH'
+}
