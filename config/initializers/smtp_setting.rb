2cbc86e52c5932be77449d7da2026d246d1571a9
tuan <tuantv.nhnd@gmail.com>
2015-06-20 11:54:37 +0700

    chang log file

--- config/initializers/smtp_setting.rb
+++ config/initializers/smtp_setting.rb
@@ -7,3 +7,13 @@ ActionMailer::Base.smtp_settings = {
     :user_name      => 'yellblog.register@gmail.com',
     :password       => 'Haianhanh1'
 }
+
+# ActionMailer::Base.smtp_settings = {
+#     :enable_starttls_auto => true,
+#     :address        => 'smtp.gmail.com',
+#     :port           => 587,
+#     :domain         => 'gmail.com',
+#     :authentication => :plain,
+#     :user_name      => 'pizika005@gmail.com',
+#     :password       => 'HAI ANHANH'
+# }
