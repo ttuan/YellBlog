196e2011e9d7c56869c956b6a0ba25458ff791a3
tuan <tuantv.nhnd@gmail.com>
2015-06-20 09:52:31 +0700

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name:  "Tuan Tran",
              email: "tuantv.nhnd@gmail.com",
              password:              "tuantuan",
              password_confirmation: "tuantuan",
-             admin: true)
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -11,5 +13,7 @@ User.create!(name:  "Tuan Tran",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
