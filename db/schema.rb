79b8dfd26c1282f10f609f32ce0756cde004274f
tuan <tuantv.nhnd@gmail.com>
2015-06-20 08:54:43 +0700

    fix bug defautl admin = true and set every not logged in user can see list user

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,11 @@ ActiveRecord::Schema.define(version: 20150619163306) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",           default: true
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
